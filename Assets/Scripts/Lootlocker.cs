40057ea086694019586968a03a61e36a7627a2e7
chris6713 <chris_6713@yahoo.com>
2022-03-04 05:11:48 -0600

    Items now stack and can move from window to window.

--- Assets/Scripts/Lootlocker.cs
+++ Assets/Scripts/Lootlocker.cs
@@ -5,61 +5,62 @@ public class Lootlocker : MonoBehaviour
 {
     void Start()
     {
-        LootLockerSDKManager.StartGuestSession((response) =>
-        {
-            if (!response.success)
-            {
-                Debug.Log("error starting LootLocker session");
+    //    return;
+    //    LootLockerSDKManager.StartGuestSession((response) =>
+    //    {
+    //        if (!response.success)
+    //        {
+    //            Debug.Log("error starting LootLocker session");
 
-                return;
-            }
+    //            return;
+    //        }
 
-            Debug.Log("successfully started LootLocker session");
-        });
+    //        Debug.Log("successfully started LootLocker session");
+    //    });
 
-        LootLockerSDKManager.SetPlayerName("Chris", (response) =>
-        {
-            if (response.success)
-            {
-                Debug.Log("Successfully set player name");
-            }
-            else
-            {
-                Debug.Log("Error setting player name");
-            }
-        });
+    //    LootLockerSDKManager.SetPlayerName("Chris", (response) =>
+    //    {
+    //        if (response.success)
+    //        {
+    //            Debug.Log("Successfully set player name");
+    //        }
+    //        else
+    //        {
+    //            Debug.Log("Error setting player name");
+    //        }
+    //    });
 
-        LootLockerSDKManager.GetInventory((response) =>
-        {
-            if (response.success)
-            {
-                Debug.Log("Successfully retrieved player inventory: " + response.inventory.Length);
+    //    LootLockerSDKManager.GetInventory((response) =>
+    //    {
+    //        if (response.success)
+    //        {
+    //            Debug.Log("Successfully retrieved player inventory: " + response.inventory.Length);
 
-                foreach(var item in response.inventory)
-                {
-                    Debug.Log("fill item with " + item.asset.name);
+    //            foreach(var item in response.inventory)
+    //            {
+    //                Debug.Log("fill item with " + item.asset.name);
 
-                    switch (item.asset.name)
-                    {
-                        case "Item_1":
-                            Inventory.Instance.inventory.Add(new Item(Item.Type.rabbit));
-                            break;
-                        case "Item_2":
-                            Inventory.Instance.inventory.Add(new Item(Item.Type.sun));
-                            break;
-                        case "Item_3":
-                            Inventory.Instance.inventory.Add(new Item(Item.Type.spade));
-                            break;
-                    }
-                }
+    //                switch (item.asset.name)
+    //                {
+    //                    case "Item_1":
+    //                        Player.Instance.backpack.AddItem(Item.Type.rabbit, 1);
+    //                        break;
+    //                    case "Item_2":
+    //                        Player.Instance.backpack.AddItem(Item.Type.sun, 1);
+    //                        break;
+    //                    case "Item_3":
+    //                        Player.Instance.backpack.AddItem(Item.Type.spade, 1);
+    //                        break;
+    //                }
+    //            }
 
-                Inventory.Instance.Backpack.RefreshItems(0);
-                Inventory.Instance.Store.RefreshItems(0);
-            }
-            else
-            {
-                Debug.Log("Error getting player inventory");
-            }
-        });
+    //            Store.Instance.window_1.RefreshItems(0);
+    //            Store.Instance.window_2.RefreshItems(0);
+    //        }
+    //        else
+    //        {
+    //            Debug.Log("Error getting player inventory");
+    //        }
+    //    });
     }
 }
